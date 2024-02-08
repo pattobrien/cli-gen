f0f3ae6b9c6d866fef44e88cd3c7e639d510cc5e
pattobrien <12474318+pattobrien@users.noreply.github.com>
2024-02-07 19:20:57 -0500

    test: End to end user method invocation test.

--- src/cli_gen/test/src/code/command/cli_method_call_builder_test.dart
+++ src/cli_gen/test/src/code/command/cli_method_call_builder_test.dart
@@ -1,13 +1,27 @@
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:checks/checks.dart';
 import 'package:test/test.dart';
 
 import '../utils/analyzer_parsers.dart';
 
 void main() {
-  group('cli method call builder ...', () {
+  group('ArgResults + User Method Invocation - int positional arg', () {
     // TODO: Implement test
     final invocationExp = generateArgResultHandlerExp();
-    test('', () {
-      print('invocationExp: $invocationExp');
+    final singleArg = invocationExp.argumentList.arguments.single;
+    test('End-to-end test', () {
+      check(singleArg).isA<MethodInvocation>()
+        ..has((p0) => p0.realTarget, 'type target')
+            .isA<SimpleIdentifier>()
+            .has((p0) => p0.name, 'type name')
+            .equals('int')
+        ..has((p0) => p0.methodName.name, 'method name').equals('parse')
+        ..has((p0) => p0.argumentList.arguments.single, 'arguments')
+            .isA<IndexExpression>()
+            .has((p0) => p0.index, 'index')
+            .isA<SimpleStringLiteral>()
+            .has((p0) => p0.value, 'value')
+            .equals('message'); // the name of the parameter
     });
   });
 }
